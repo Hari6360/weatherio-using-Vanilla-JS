fc5ff0e466c7f0b12f8745ae3f9321fad977bba8
Hari Prasad <101623030+Hari6360@users.noreply.github.com>
2023-08-16 18:51:47 +0530

    Initial commit

---
+++ assets/js/route.js
@@ -0,0 +1,46 @@
+"use strict";
+
+import { updateWeather, error404 } from "./app.js";
+const defaultLocation = "#/weather?lat=51.5073219&lon=-0.1276474";
+
+// funtion to get location from user using browser inbuilt functionality
+
+const currentLocation = function () {
+	window.navigator.geolocation.getCurrentPosition(
+		(res) => {
+			const { latitude, longitude } = res.coords;
+
+			updateWeather(`lat=${latitude}`, `lon=${longitude}`);
+		},
+		(err) => {
+			window.location.hash = defaultLocation;
+		}
+	);
+};
+
+// function to get location from user based on user search
+
+const searchedLocation = (query) => updateWeather(...query.split("&"));
+
+const routes = new Map([
+	["/current-location", currentLocation],
+	["/weather", searchedLocation],
+]);
+
+const checkHash = function () {
+	const requestURL = window.location.hash.slice(1);
+	const [route, query] = requestURL.includes
+		? requestURL.split("?")
+		: [requestURL];
+	routes.get(route) ? routes.get(route)(query) : error404();
+};
+
+window.addEventListener("hashChange", checkHash);
+
+window.addEventListener("load", function () {
+	if (!this.window.location.hash) {
+		this.window.location.hash = "#/current-location";
+	} else {
+		checkHash();
+	}
+});
